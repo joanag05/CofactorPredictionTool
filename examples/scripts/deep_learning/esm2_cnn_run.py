8e0c323b0997f58d0240b490bf8cdf4f407bdac0
id9411@alunos.uminho.pt <id9411@alunos.uminho.pt>
2024-08-06 16:20:50 +0100

    [FIX] cnn_esm.py & cnn_seqvec.py merged into cnn_model.py; [FIX] paths in cnn_model.py [FIX] deep_learning.py to deep_learning [FIX] requirements.txt [DEL] installation egg (cofactor_prediction_tool.egg-info

---
+++ examples/scripts/deep_learning/esm2_cnn_run.py
@@ -0,0 +1,42 @@
+import shutil
+from os.path import join
+
+import torch
+
+from cofactor_prediction_tool.deep_learning.cnn_model import load_data, run, select_thresholds, hpo_optuna
+import random
+import numpy as np
+
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(42)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    dataset_path = r"/home/emanuel/PythonProjects/CofactorPredictionTool/data/Final/ESM2/esm2.h5"
+    (dataloaders, dataset_sizes, dataset_shape, X_test, X_test_tensor, y_test_tensor, labels, weights) = load_data(dataset_path)
+    data_path = "/home/emanuel/PythonProjects/CofactorPredictionTool/data/Final/ESM2/cnn"
+    # Run the model
+    model, f1_score, f1_score_sklearn_res = run(device=device, labels=labels, dataset_shape=dataset_shape, dataloaders=dataloaders, dataset_sizes=dataset_sizes,
+                                                x_test_tensor=X_test_tensor, y_test_tensor=y_test_tensor, data_path=data_path)
+    print(f1_score_sklearn_res)
+    # # Save the best model
+    torch.save(model, join(data_path, "best_cnn.pth"))
+    #
+    # # Select the optimal thresholds
+    select_thresholds(X_test, y_test_tensor, data_path=data_path, device=device, labels=labels)
+
+    # Perform hyperparameter optimization using Optuna
+    hpo_optuna(device=device, labels=labels, dataset_shape=dataset_shape, dataloaders=dataloaders, dataset_sizes=dataset_sizes, x_test_tensor=X_test_tensor,
+               y_test_tensor=y_test_tensor, model_name="bestesm2_hpo", data_path=data_path)
+
+    shutil.copy(join(data_path, "bestesm2_hpo.pth"), "../../../src/cofactor_prediction_tool/resources/")
+
+
+if __name__ == "__main__":
+    main()
