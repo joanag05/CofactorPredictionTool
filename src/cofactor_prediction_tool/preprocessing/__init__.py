e4748cb2cdbad16faf26273ab71eb8e3bede3eaa
joanag05 <pg49835@alunos.uminho.pt>
2024-09-03 11:53:31 +0100

    [CHANGE] init.py

--- src/cofactor_prediction_tool/preprocessing/__init__.py
+++ src/cofactor_prediction_tool/preprocessing/__init__.py
@@ -1,6 +1,4 @@
 from .Preprocessing import Preprocessing
-from .ProteinCofactorsDescriptors import ProteinCofactorsDescriptors
-#from ..secqvec.embeddingsseq2vec import EmbeddingsSeq2Vec
 from .EmbeddingsESM2 import EmbeddingsESM2
 from .EmbeddingsProtFlash import EmbeddingsProtFlash
-from .FeatureSelection import UnvariateSelector
+
